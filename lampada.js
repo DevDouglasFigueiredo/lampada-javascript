1b453ea49015b0f5b32946a0bc0d8236edf0e4d8
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-03-04 17:38:59 -0300

    lampada javascript

--- lampada.js
+++ lampada.js
@@ -2,6 +2,7 @@ const turnOn = document.getElementById ("turnOn");
 const turnOff = document.getElementById ("turnOff");
 const lamp = document.getElementById ("lamp");
 
+
 function isLampBroken (){
     return lamp.src.indexOf ('quebrad') > -1;
 }
@@ -27,7 +28,5 @@ turnOn.addEventListener("click", lampOn);
 turnOff.addEventListener('click' , lampOff);
 lamp.addEventListener('mouseover' , lampOn);
 lamp.addEventListener('mouseleave' , lampOff);
-lamp.addEventListener('dblclick' , lampBroken)
-
-
+lamp.addEventListener('dblclick' , lampBroken);
 
