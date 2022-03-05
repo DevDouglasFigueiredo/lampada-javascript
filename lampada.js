d5a15d722a6bfc852b46f9ba54cc886fcb683dac
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-03-05 18:03:29 -0300

    Lampada JavaScript

--- lampada.js
+++ lampada.js
@@ -3,14 +3,18 @@ const turnOff = document.getElementById ("turnOff");
 const lamp = document.getElementById ("lamp");
 
 
+
 function isLampBroken (){
     return lamp.src.indexOf ('quebrad') > -1;
+    
 }
 
 function lampOn(){
     if( !isLampBroken ()){
     lamp.src = './img/ligad.png';
-    
+    var lightOn = window.document.body
+    lightOn.style.background = 'rgb(250, 250, 117)';
+   
     }
     
 }
@@ -18,12 +22,18 @@ function lampOn(){
 function lampOff(){
     if( !isLampBroken ()){
     lamp.src = './img/desligad.png';
+    var lightOff = window.document.body
+    lightOff.style.background = 'rgb(20, 20, 20)';
     }
 }
 
 function lampBroken(){
     lamp.src = './img/quebrad.png';
+    var lightOn = window.document.body
+    lightOn.style.background = 'rgb(250, 250, 117)';
+
 }
+
 
 turnOn.addEventListener("click", lampOn);
 turnOff.addEventListener('click' , lampOff);
