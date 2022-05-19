90e5cdf68b2f855bb5a74f4c31e5facb35552912
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-05-19 13:45:03 -0300

    new feat added

--- lampada.js
+++ lampada.js
@@ -1,38 +1,41 @@
 const turnOn = document.getElementById ("turnOn");
 const turnOff = document.getElementById ("turnOff");
 const lamp = document.getElementById ("lamp");
+const title = document.getElementById("title");
 
 
 function isLampBroken (){
     return lamp.src.indexOf ('quebrad') > -1;
+    
     
 }
 
 function lampOn(){
     if( !isLampBroken ()){
     lamp.src = './img/ligad.png';
-    var lightOn = window.document.body
-    lightOn.style.background = 'rgb(250, 250, 117)';
-   
-    }
-    
+    var lightOn = window.document.body;
+    lightOn.style.background = '#f7d12c';
+    title.style.color = '#000000';
+    } 
+        
 }
 
 function lampOff(){
     if( !isLampBroken ()){
     lamp.src = './img/desligad.png';
-    var lightOff = window.document.body
-    lightOff.style.background = 'rgb(20, 20, 20)';
-
+    var lightOff = window.document.body;
+    lightOff.style.background = '#0e0d0d';
+    title.style.color = '#FFFFFF';
     }
     
 }
 
 function lampBroken(){
     lamp.src = './img/quebrad.png';
-    var lightOn = window.document.body
-    lightOn.style.background = 'rgb(250, 250, 117)';
-
+    var lightOff = window.document.body;    
+    lightOff.style.background = '#0e0d0d';
+    title.style.color = '#FFFFFF';
+    title.textContent = "A lampada quebrou!!!";
 }
 
 
